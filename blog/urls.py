46f739d602e88648dbc40e3f70767350627c2dc0
Tim Jones <timjonez@protonmail.com>
2020-07-13 21:21:07 -0400

    [blog/urls] add wagtail urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,23 +1,12 @@
-"""blog URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.core import urls as wagtail_urls
+from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
@@ -29,6 +18,9 @@ urlpatterns = [
     path('contact/', include('contact_us.urls', namespace='contact')),
     path('prayers/', include('prayers.urls', namespace='prayers')),
     path('summernote/', include('django_summernote.urls')),
+    re_path(r'^cms/', include(wagtailadmin_urls)),
+    re_path(r'^documents/', include(wagtaildocs_urls)),
+    re_path(r'^pages/', include(wagtail_urls)),
 
 ]
 if settings.DEBUG:
